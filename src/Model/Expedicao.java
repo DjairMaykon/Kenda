638fef7936d6a5d505f909ce2bf57413c710b6f4
Kaione <Kaione@10.17.11.102>
2018-01-29 17:07:27 -0300

    Classe expediçao.

--- src/Model/Expedicao.java
+++ src/Model/Expedicao.java
@@ -5,7 +5,10 @@
  */
 package Model;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
@@ -15,11 +18,12 @@ public class Expedicao {
     private Date dataEntra;
     private Date dataSai; 
     private String situacao;
-    private int codExped;
+    private int codigo;
     private int codPed;
     private int codTransp;
     private String descricao;
     private double custo;
+    private Connection con;
 
     public Date getDataEntra() {
         return dataEntra;
@@ -46,11 +50,11 @@ public class Expedicao {
     }
 
     public int getCodExped() {
-        return codExped;
+        return codigo;
     }
 
     public void setCodExped(int codExped) {
-        this.codExped = codExped;
+        this.codigo = codExped;
     }
 
     public int getCodPed() {
@@ -83,6 +87,97 @@ public class Expedicao {
 
     public void setCusto(double custo) {
         this.custo = custo;
+    }
+    
+    
+    
+    public void adicionar(){
+    
+        PreparedStatement pstmt = null;
+        
+        String sql = "INSERT INTO ESPEDICAO(cod, custo, cod_pedido, cod_transportadoras, descricao, data_de_saida, data_de_entrega,situacao) VALUES(?,?,?,?,?,?,?,?)"; // banco
+        
+        try {
+            
+            con = new ConnectionFactory().getConnection();
+            pstmt = con.prepareStatement(sql);
+        
+            pstmt.setInt(1, codigo); //strings /\
+            pstmt.setDouble(2, custo);
+            pstmt.setInt(3, codPed);
+            pstmt.setInt(4, codTransp);
+            pstmt.setString(5, descricao);
+            pstmt.setDate(6, dataSai);
+            pstmt.setDate(7, dataEntra);
+            pstmt.setString(5, situacao);
+           
+            
+            
+            pstmt.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            
+            throw new RuntimeException("ERRO AO ADICIONAR\n" + ex);
+            
+        }   
+        
+    }
+    
+    public void deletar(){
+    
+        PreparedStatement pstmt = null;
+        
+        String sql = "DELETE FROM ESPEDICAO WHERE cod = ?"; // banco 
+        
+        try {
+            
+            con = new ConnectionFactory().getConnection();
+            pstmt = con.prepareStatement(sql);
+        
+            pstmt.setInt(1, codigo);
+            
+            pstmt.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            
+            throw new RuntimeException("ERRO AO ADICIONAR\n" + ex);
+            
+        }   
+        
+    }
+    
+     public void alterar(){
+    
+        PreparedStatement pstmt = null;
+        
+        String sql = "UPDATE FROM fornecedores SET custo=?, cod_pedido=?, cod_transportadoras=?, descricao=?, data_de_saida=?, data_de_entrega=?,situacao? where cod=?"; // banco  
+        
+        try {
+            
+            con = new ConnectionFactory().getConnection();
+            pstmt = con.prepareStatement(sql);
+        
+           //strings /\
+            pstmt.setDouble(1, custo);
+            pstmt.setInt(2,codPed);
+            pstmt.setInt(3, codTransp);
+            pstmt.setString(4, descricao);
+            pstmt.setDate(5, dataSai);
+            pstmt.setDate(6, dataEntra);
+            pstmt.setString(7, situacao);
+            pstmt.setInt(8,codigo);
+            
+            pstmt.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            
+            throw new RuntimeException("ERRO AO Alterar\n" + ex);
+            
+        }   
+        
     }
 
    
