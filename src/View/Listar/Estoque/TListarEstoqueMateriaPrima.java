7455e872667b1d8d3669bd98c09246d4a224f569
Djair Maykon <Djair Maykon@192.168.80.108>
2018-01-29 11:21:23 -0300

    Arrumei telas inicial, login, ferramentas, listar cliente estoque e expedicao

--- src/View/Listar/Estoque/TListarEstoqueMateriaPrima.java
+++ src/View/Listar/Estoque/TListarEstoqueMateriaPrima.java
@@ -38,7 +38,7 @@ public class TListarEstoqueMateriaPrima extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTListarEstoqueMateriaPrima = new javax.swing.JTable();
-        jTFListarEstoqueMateriaPrima = new javax.swing.JTextField();
+        jTFEntradaPesquisa = new javax.swing.JTextField();
         jBBusca = new javax.swing.JButton();
 
         setClosable(true);
@@ -68,7 +68,7 @@ public class TListarEstoqueMateriaPrima extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addComponent(jScrollPane1)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTFListarEstoqueMateriaPrima, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jTFEntradaPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jBBusca, javax.swing.GroupLayout.PREFERRED_SIZE, 172, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -78,7 +78,7 @@ public class TListarEstoqueMateriaPrima extends javax.swing.JInternalFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTFListarEstoqueMateriaPrima, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTFEntradaPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBBusca))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -109,7 +109,7 @@ public class TListarEstoqueMateriaPrima extends javax.swing.JInternalFrame {
     private javax.swing.JButton jBBusca;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTFListarEstoqueMateriaPrima;
+    private javax.swing.JTextField jTFEntradaPesquisa;
     private javax.swing.JTable jTListarEstoqueMateriaPrima;
     // End of variables declaration//GEN-END:variables
 }
