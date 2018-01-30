efd8d6c6de9471cf2880bb2e717bac49eaf71841
Kaione <Kaione@10.17.11.102>
2018-01-30 09:51:42 -0300

    CONTROLESS

---
+++ src/Control/ControleListarFuncionario.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import View.Listar.Funcionario.TListarFuncionario;
+
+
+
+
+
+
+
+
+
+/**
+ *
+ * @author Djair Maykon
+ */
+class ControleListarFuncionario {
+    
+    private TListarFuncionario telaListarFuncionario;
+
+    public ControleListarFuncionario() {
+    
+        telaListarFuncionario = new TListarFuncionario();
+    
+    }
+
+    public TListarFuncionario gettelaListarFuncionario() {
+        return telaListarFuncionario;
+    }
+    
+}
