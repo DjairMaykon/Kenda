6a2bc8b57bc61a0151971ebf9df15a6ab409febf
Matheus <Matheus@DESKTOP-K8OF21F>
2018-01-29 23:39:03 -0200

    Intanciar telascadastrar

---
+++ src/Control/ControleCadastrarTinta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+import View.TelasCadastrar.TCadastrarTinta;
+/**
+ *
+ * @author Matheus
+ */
+public class ControleCadastrarTinta {
+    
+    private TCadastrarTinta cadastrarTinta= new TCadastrarTinta(); 
+    
+    
+    
+}
