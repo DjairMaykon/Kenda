9dba03b18130270eae47475279ab793334a5181b
Emanuele <Emanuele@Manu-PC>
2018-01-31 09:54:10 -0200

    Telas Listar.

---
+++ src/Control/ControleListarCliente.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+import View.Listar.Cliente.TListarCliente;
+
+/**
+ *
+ * @author Matheus
+ */
+public class ControleListarCliente {
+    
+    private TListarCliente telaListarCliente;
+    
+    
+     public ControleListarCliente() {
+    
+       telaListarCliente = new TListarCliente();
+    
+    }
+
+    public TListarCliente getListarCliente() {
+        return telaListarCliente;
+    }
+
+   
+    
+    
+    
+}
