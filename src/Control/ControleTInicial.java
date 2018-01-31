48b97da23da070af28c50d25e321736887732a2f
Emanuele <Emanuele@10.17.13.245>
2018-01-31 11:04:26 -0200

    Eventos de botão.

--- src/Control/ControleTInicial.java
+++ src/Control/ControleTInicial.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Control;
 
 import Model.MUsuario;
@@ -122,6 +117,97 @@ class ControleTInicial {
             }
         });
         
+          
+        telaInicial.getjMIListarFuncionarios().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarFuncionario(e);
+            }
+        });
+        
+         telaInicial.getjSMIEstoqueFornecedores().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarEstoqueFornecedores(e);
+            }
+        });
+         
+          telaInicial.getjSMIEstoqueMateriaPrima().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarEstoqueMateriaPrima(e);
+            }
+        });
+          
+           telaInicial.getjSMIExpedicaoEmAndamento().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarExpedicaoEmAndamento(e);
+            }
+        });
+           
+           telaInicial.getjSMIExpedicaoFinalizados().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarExpedicaoFinalizados(e);
+            }
+        });
+         
+             telaInicial.getjSMIExpedicaoTransportadoras().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarExpedicaoTransportadora(e);
+            }
+        });
+             
+              telaInicial.getjSMIPedidosEmExpedição().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarPedidosExpedicao(e);
+            }
+        });
+              
+              
+               telaInicial.getjSMIPedidosEmLaboratorio().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarPedidosLaboratorio(e);
+            }
+        });
+               
+                 
+               telaInicial.getjSMIPedidosEmProducao().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarPedidosProducao(e);
+            }
+        });
+               
+                  telaInicial.getjSMIPedidosFinalizados().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarPedidosFinalizado(e);
+            }
+        });
+           
+            
+            telaInicial.getjSMIProducaoFinalizados().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarProducaoFinalizados(e);
+            }
+        });
+            
+            telaInicial.getjSMIProducaoEmAndamento().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                acaoMIListarProducaoEmAndamento(e);
+            }
+        });
+               
+         
+               
+         
          
     }
     
@@ -250,6 +336,88 @@ class ControleTInicial {
         cListarEstoqueMateriaPrima.gettelaListarEstoqueMateriaPrima().setVisible(true);
         
     }
+     
+     
+       public void acaoMIListarExpedicaoEmAndamento(ActionEvent evt){
+        
+        ControleListarExpedicaoEmAndamento cListarExpedicaoEmAndamento = new ControleListarExpedicaoEmAndamento();
+        telaInicial.getjDPPrincipal().add(cListarExpedicaoEmAndamento.gettelaListarExpedicaoEmAndamento());
+        cListarExpedicaoEmAndamento.gettelaListarExpedicaoEmAndamento().setVisible(true);
+        
+    }
+       
+        public void acaoMIListarExpedicaoFinalizados(ActionEvent evt){
+        
+        ControleListarExpedicaoFinalizados cListarExpedicaoFinalizados = new ControleListarExpedicaoFinalizados();
+        telaInicial.getjDPPrincipal().add(cListarExpedicaoFinalizados.gettelaListarExpedicaoFinalizados());
+        cListarExpedicaoFinalizados.gettelaListarExpedicaoFinalizados().setVisible(true);
+        
+    }
+        
+        
+         public void acaoMIListarExpedicaoTransportadora(ActionEvent evt){
+        
+        ControleListarExpedicaoTransportadora cListarExpedicaoTransportadora = new ControleListarExpedicaoTransportadora();
+        telaInicial.getjDPPrincipal().add(cListarExpedicaoTransportadora.gettelaListarExpedicaoTransportadora());
+        cListarExpedicaoTransportadora.gettelaListarExpedicaoTransportadora().setVisible(true);
+        
+    }
+         
+         
+        public void acaoMIListarPedidosExpedicao(ActionEvent evt){
+        
+        ControleListarPedidosExpedicao cListarPedidosExpedicao = new ControleListarPedidosExpedicao();
+        telaInicial.getjDPPrincipal().add(cListarPedidosExpedicao.gettelaListarPedidosExpedicao());
+        cListarPedidosExpedicao.gettelaListarPedidosExpedicao().setVisible(true);
+        
+    }
+           
+           
+      public void acaoMIListarPedidosFinalizado(ActionEvent evt){
+        
+        ControleListarPedidosFinalizado cListarPedidosFinalizado = new ControleListarPedidosFinalizado();
+        telaInicial.getjDPPrincipal().add(cListarPedidosFinalizado.gettelaListarPedidosFinalizado());
+        cListarPedidosFinalizado.gettelaListarPedidosFinalizado().setVisible(true);
+        
+    }
+      
+      
+      public void acaoMIListarPedidosLaboratorio(ActionEvent evt){
+        
+        ControleListarPedidosLaboratorio cListarPedidosLaboratorio = new ControleListarPedidosLaboratorio();
+        telaInicial.getjDPPrincipal().add(cListarPedidosLaboratorio.gettelaListarPedidosLaboratorio());
+        cListarPedidosLaboratorio.gettelaListarPedidosLaboratorio().setVisible(true);
+        
+    }
+      
+      public void acaoMIListarPedidosProducao(ActionEvent evt){
+        
+        ControleListarPedidosProducao cListarPedidosProducao = new ControleListarPedidosProducao();
+        telaInicial.getjDPPrincipal().add(cListarPedidosProducao.gettelaListarPedidosProducao());
+        cListarPedidosProducao.gettelaListarPedidosProducao().setVisible(true);
+        
+    }
+      
+       public void acaoMIListarProducaoEmAndamento(ActionEvent evt){
+        
+        ControleListarProducaoEmAndamento cListarProducaoEmAndamento = new ControleListarProducaoEmAndamento();
+        telaInicial.getjDPPrincipal().add(cListarProducaoEmAndamento.gettelaListarProducaoEmAndamento());
+        cListarProducaoEmAndamento.gettelaListarProducaoEmAndamento().setVisible(true);
+        
+    }
+       
+       public void acaoMIListarProducaoFinalizados(ActionEvent evt){
+        
+        ControleListarProducaoFinalizados cListarProducaoFinalizados = new ControleListarProducaoFinalizados();
+        telaInicial.getjDPPrincipal().add(cListarProducaoFinalizados.gettelaListarProducaoFinalizados());
+        cListarProducaoFinalizados.gettelaListarProducaoFinalizados().setVisible(true);
+        
+    }
+    
+      
+      
+    
+    
     
     
     
